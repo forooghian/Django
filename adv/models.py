5b40db7080c2d1cf72dab8f0933dcd046814da6e
forooghian <e.forooghian@gmail.com>
2022-09-20 23:16:18 +0430

    create category and adv models
    category and adv models were created. Also phonenumber app was installed for creating phone number field

--- adv/models.py
+++ adv/models.py
@@ -1,3 +1,14 @@
+from pyexpat import model
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Adv(models.Model):
+    title = models.CharField(max_length=255)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    price = models.DecimalField(max_digits=12, decimal_places=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
