6cbbbfdd5734836e333576de4910695802c5c892
forooghian <e.forooghian@gmail.com>
2022-09-21 00:34:04 +0430

    register adv and category models in admin panel

--- adv/models.py
+++ adv/models.py
@@ -1,6 +1,7 @@
 from pyexpat import model
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -12,3 +13,4 @@ class Adv(models.Model):
     phone = PhoneNumberField(null=False, blank=False, unique=True)
     price = models.DecimalField(max_digits=12, decimal_places=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
