abae3d8455a31b916eca66ef300f2dcc7bd0355b
Elyas Forooghian <e.forooghian@gmail.com>
2023-05-24 09:18:58 +0330

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework.apps.RestFrameworkConfig",
-    "divar_app.apps.DivarAppConfig"
+    "divar_app.apps.DivarAppConfig",
+    "divar_app.apps.dashboardConfig"
 ]
 
 MIDDLEWARE = [
